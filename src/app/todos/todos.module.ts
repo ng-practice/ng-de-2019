2b3691cc89b6225312709e00af1f7b83c2989578
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-08-18 14:17:06 +0200

    fix: adjust BEM notation

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TodoCheckerComponent } from './todo-checker/todo-checker.component';
+import { TodoQuickAddComponent } from './todo-quick-add/todo-quick-add.component';
 
 @NgModule({
-  declarations: [TodoCheckerComponent],
+  declarations: [TodoCheckerComponent, TodoQuickAddComponent],
   imports: [CommonModule],
   exports: [TodoCheckerComponent]
 })
