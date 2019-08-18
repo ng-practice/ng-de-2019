fd61c1b97af518d64683bf7d1b0a55d612ecdfcf
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-08-18 13:32:14 +0200

    feat: add theme for checkbox

--- src/app/todos/todo-check/todo-check.component.ts
+++ src/app/todos/todo-check/todo-check.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Todo } from '../models';
 
 @Component({
   selector: 'nde-todo-check',
   templateUrl: './todo-check.component.html',
   styleUrls: ['./todo-check.component.scss']
 })
-export class TodoCheckComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+export class TodoCheckComponent {
+  @Input() todo: Todo;
 }
