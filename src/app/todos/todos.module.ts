9846c514370c09c0775f6c338a9ee247f3ea0e16
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-08-20 07:07:18 +0200

    feat: configure routing

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TodoCheckerComponent } from './todo-checker/todo-checker.component';
-import { TodoQuickAddComponent } from './todo-quick-add/todo-quick-add.component';
-import { TodosComponent } from './todos.component';
 import { TodoCounterComponent } from './todo-counter/todo-counter.component';
+import { TodoQuickAddComponent } from './todo-quick-add/todo-quick-add.component';
+import { TodosRoutingModule } from './todos-routing.module';
+import { TodosComponent } from './todos.component';
 
 @NgModule({
-  declarations: [TodoCheckerComponent, TodoQuickAddComponent, TodosComponent, TodoCounterComponent],
-  imports: [CommonModule],
+  declarations: [
+    TodoCheckerComponent,
+    TodoQuickAddComponent,
+    TodosComponent,
+    TodoCounterComponent
+  ],
+  imports: [CommonModule, TodosRoutingModule],
   exports: [TodosComponent]
 })
 export class TodosModule {}
