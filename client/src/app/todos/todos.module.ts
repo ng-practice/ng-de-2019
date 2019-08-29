d1ed8d80171fd31e36e24f56bb0b3b3ada40d946
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-08-29 14:43:48 +0200

    solve task 11

--- client/src/app/todos/todos.module.ts
+++ client/src/app/todos/todos.module.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { TodoCheckerComponent } from './todo-checker/todo-checker.component';
 import { TodoQuickAddComponent } from './todo-quick-add/todo-quick-add.component';
@@ -6,7 +7,7 @@ import { TodosComponent } from './todos.component';
 
 @NgModule({
   declarations: [TodosComponent, TodoQuickAddComponent, TodoCheckerComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   exports: [TodosComponent]
 })
 export class TodosModule {}
